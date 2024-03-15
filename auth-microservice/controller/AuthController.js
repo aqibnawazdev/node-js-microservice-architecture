c520bb7f5754dd03ff988d01d7d840782c982b6c
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-03-15 14:10:09 +0500

    login

--- auth-microservice/controller/AuthController.js
+++ auth-microservice/controller/AuthController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import prisma from "../config/db.config.js"
+import jwt from "jsonwebtoken"
 class AuthController {
     static async register(req, res) {
         const payload = req.body
@@ -16,6 +17,35 @@ class AuthController {
         } catch (error) {
             res.status(404).send(error)
         }
+    }
+    static async login(req, res) {
+        const { email, password } = req.body
+
+        try {
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: email
+                }
+            })
+
+            if (user) {
+                const verify = bcrypt.compare(user.password, password)
+                if (!verify) {
+                    return res.status(400).send({ message: "Invalid email or Password..." })
+                }
+                const payload = { email: user.email, id: user._id }
+                const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                    expiresIn: "365d"
+                })
+
+                res.status(200).send({ message: "Logged in successfull...", access_token: `Bearer ${token}`, data: user })
+            }
+            return res.status(501).send({ message: "Invlaid credentials..." })
+        } catch (error) {
+            res.status(501).send({ message: "something went wrong..." })
+        }
+
     }
 }
 
